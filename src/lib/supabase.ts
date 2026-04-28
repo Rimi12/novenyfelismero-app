6213fbe4663d02cf36b7c08352586727c605d916
Rimi12 <baro37@gmail.com>
2026-04-28 09:17:12 +0200

    Supabase URL eliras javitas

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hdkryigvnzdbymaxwirxs.supabase.co';
+const supabaseUrl = 'https://hdkryigvnzdbymawirxs.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhka3J5aWd2bnpkYnltYXdpcnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzIzNTcsImV4cCI6MjA5Mjg0ODM1N30.XJ6tp0BIscdbZCoSlshA7113D11lEqqB-VLV92OpIZk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
